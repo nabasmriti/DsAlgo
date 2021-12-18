b6778f2bae377d8fed2efb98cced27bf49f5b78d
Nabasmriti Ghosh <nabnip.beena123@gmail.com>
2021-12-18 22:07:04 +0530

    Pascal's Triangle

--- src/others/PascalTriangle.java
+++ src/others/PascalTriangle.java
@@ -1,5 +1,25 @@
 package others;
 
-public class PascalTriangle {
+import java.util.LinkedList;
+import java.util.List;
 
+class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<Integer> row, prev = null;
+        List<List<Integer>> result = new LinkedList<>();
+        for(int i =0; i<numRows ; i++){
+            row = new LinkedList<>();
+            for(int j =0 ; j<=i; j++){
+                if(j == 0 || j == i)
+                    row.add(1);
+                else{
+                    row.add(prev.get(j-1)+prev.get(j));
+
+                }
+            }
+            result.add(row);
+            prev = row;
+        }
+        return result;
+    }
 }
