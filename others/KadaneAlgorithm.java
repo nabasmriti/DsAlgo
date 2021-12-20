5770ce8b0f07ad97372dda4e87c6fae3a8228973
Nabasmriti Ghosh <nabnip.beena123@gmail.com>
2021-12-20 21:37:38 +0530

    Largest sum contiguous subarray

--- others/KadaneAlgorithm.java
+++ others/KadaneAlgorithm.java
@@ -1,5 +1,45 @@
 package others;
 
-public class KadaneAlgorithm {
+import java.util.ArrayList;
 
+/*Given an integer array nums, find the contiguous subarray 
+ * (containing at least one number) which has the largest sum and return its sum.
+ * Kadane O(n) time and O(1) space*/
+public class KadaneAlgorithm {
+	public static int maxSubArray(int[] nums,ArrayList<Integer> subarray) { 
+		int msf=nums[0] , meh=0 ; 
+		int s=0;
+		for(int i=0;i<nums.length;i++){ 
+		meh+=nums[i]; 
+		if(meh>msf)
+		{ 
+		    subarray.clear();
+		    msf=meh; 
+		    subarray.add(s); 
+		    subarray.add(i); 
+		    
+		} 
+		if(meh<0)
+		{
+		    meh=0; 
+		    s=i+1;
+		    
+		} 
+		} 
+		 
+		return msf; 
+		} 
+		    public static void main(String args[])
+		    {
+		        int arr[]={-2,1,-3,4,-1,2,1,-5,4};
+		        ArrayList<Integer> subarray=new ArrayList<>();
+		        int lon=maxSubArray(arr,subarray);
+		        System.out.println("The longest subarray with maximum sum is "+lon);
+		        System.out.println("The subarray is ");
+		        for(int i=subarray.get(0);i<=subarray.get(1);i++)
+		        {
+		            System.out.print(arr[i]+" ");
+		        }
+		        
+		    }
 }
