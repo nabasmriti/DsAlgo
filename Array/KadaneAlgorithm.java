8aad27143b8aa27b1fc939c10a29f711c1390283
Nabasmriti Ghosh <nabnip.beena123@gmail.com>
2021-12-23 23:01:37 +0530

    Majority element

--- Array/KadaneAlgorithm.java
+++ Array/KadaneAlgorithm.java
@@ -1,4 +1,4 @@
-package others;
+package Array;
 
 import java.util.ArrayList;
 
---
+++ Array/MajorityElement.java
@@ -0,0 +1,19 @@
+package Array;
+//Boyer Moore's Voting Element
+public class MajorityElement {
+	 public int majorityElement(int[] nums) {
+	        int count = 0;
+	        int candidate = 0;
+
+	        for (int num : nums) {
+	            if (count == 0) {
+	                candidate = num;
+	            }
+	            if(num==candidate)
+	            count += 1; 
+	            else count -= 1; 
+	        }
+
+	        return candidate;
+	    }
+}
