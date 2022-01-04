0eb91d765fb47f3dcab983865f355816a8d35981
Nabasmriti Ghosh <nabnip.beena123@gmail.com>
2022-01-04 23:02:46 +0530

    Stock buy and sell

--- others/StockBuySell.java
+++ others/StockBuySell.java
@@ -1,5 +1,13 @@
 package others;
 
 public class StockBuySell {
-
+	 public int maxProfit(int[] prices) {
+	        int minValue = Integer.MAX_VALUE;
+	        int maxDiff = 0;
+	        for(int i =0; i< prices.length; i++){
+	            minValue = Math.min(minValue,prices[i]);
+	            maxDiff = Math.max(maxDiff,prices[i] - minValue);
+	        }
+	        return maxDiff;
+	    }
 }
