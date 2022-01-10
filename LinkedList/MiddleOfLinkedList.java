a466a207c556ab860d3df852c3f35ed2e64d895d
NAGHOSH <NAGHOSH@NAGHOSH-X40-J.in.oracle.com>
2022-01-10 21:33:53 +0530

    ReverseLinkedList , Middle of Linked list

---
+++ LinkedList/MiddleOfLinkedList.java
@@ -0,0 +1,26 @@
+/*
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+*/
+
+public class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode() {}
+	      ListNode(int val) { this.val = val; }
+	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	  }
+class MiddleOfLinkedList {
+    public ListNode middleNode(ListNode head) {
+        if(head == null)
+            return head;
+        ListNode temp1 = head;
+        ListNode temp2 = head;
+        while(temp2 != null && temp2.next != null){
+            temp2 = temp2.next.next;
+            temp1 = temp1.next;
+        }
+        return temp1;
+    }
+}
---
+++ LinkedList/ReverseLinkedList.java
@@ -0,0 +1,29 @@
+/*Given the 
+ * head of a singly linked list, reverse the list, and return the reversed list.*/
+ */
+ 
+ public class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode() {}
+	      ListNode(int val) { this.val = val; }
+	      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	  }
+ public class ReverseLinkedList{
+	 public ListNode reverseList(ListNode head) {
+		    
+	        ListNode curr = head;
+	        ListNode prev = null;
+	        while(curr !=null)
+	        {
+	            if(curr.next == null){
+	                head = curr;
+	            }
+	            ListNode temp = curr.next;
+	            curr.next = prev;
+	            prev = curr;
+	            curr = temp;
+	        }
+	        return head;
+	    }
+ }
