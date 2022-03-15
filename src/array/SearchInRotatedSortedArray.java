ee4983ffb1bdfd76af6fadbeb54c224cc87f3c2c
nabasmriti <nabnip.beena123@gmail.com>
2022-03-15 14:43:48 +0530

    Search In Rotated Sorted Array

---
+++ src/array/SearchInRotatedSortedArray.java
@@ -0,0 +1,26 @@
+package array;
+
+public class SearchInRotatedSortedArray {
+	 public int search(int[] nums, int target) {
+	        int low = 0, high = nums.length -1, mid;
+	        
+	        while(low<=high){
+	            mid = (low+high)/2;
+	            if(nums[mid] == target)
+	                return mid;
+	            if(nums[low] <= nums[mid]){
+	                if(nums[low] <= target && nums[mid] >=target)
+	                    high = mid-1;
+	                else
+	                    low = mid+1;
+	                
+	            }else{
+	                if(nums[mid]<= target && target <= nums[high])
+	                    low  = mid+1;
+	                else
+	                    high = mid-1;
+	            }
+	        }
+	        return -1;
+	    }
+}
